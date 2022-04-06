4d0e845b1bd1db5198eaecf13291e77827684d16
amolodoyy <amolnikita1@gmail.com>
2022-04-06 18:31:43 +0300

    Reg and Login UTests

---
+++ UnitTests/RegisterTest.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebCbt_Backend.Controllers;
+namespace UnitTests
+{
+    [TestClass]
+    public class RegisterTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange 
+            var controller = UsersController();
+            // Act
+
+            // Assert
+        }
+    }
+}
