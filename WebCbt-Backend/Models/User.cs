04471adc38c2a4cacb210a6241809b84ee6a7200
cutieskye <68032402+cutieskye@users.noreply.github.com>
2022-04-06 01:22:51 +0000

    Add ASP.NET Core Identity

---
+++ WebCbt-Backend/Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace WebCbt_Backend.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+
+        public string Login { get; set; } = "";
+
+        public string Password { get; set; } = "";
+
+        public int Age { get; set; }
+
+        public string Gender { get; set; } = "";
+
+        public int UserStatus { get; set; }
+
+        public bool Banned { get; set; }
+    }
+}
