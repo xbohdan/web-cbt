51481b971cacda5b1dada38bc1cd3d382d022668
Akmal <akmalmuxiddinov@mail.ru>
2022-05-08 18:11:24 +0200

    Add header button for authorized users
    Implement logout on frontend

--- webcbt-frontend/src/components/Header/Header.tsx
+++ webcbt-frontend/src/components/Header/Header.tsx
@@ -1,17 +1,67 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import {Typography} from 'antd';
+import {LogoutOutlined, SettingOutlined, UserOutlined} from '@ant-design/icons';
+import React, {useState} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import {Popover, Typography, Button, Avatar} from 'antd';
 
 import './Header.css';
+import useAppDispatch from '../../hooks/useAppDispatch';
+import useAppSelector from '../../hooks/useAppSelector';
+import selectIsAuth from '../../store/user/selectors/selectIsAuth';
+import selectLogin from '../../store/user/selectors/selectLogin';
+import {logout} from '../../store/user/slice';
 
 const {Title} = Typography;
 
 const Header = () => {
+  const isAuth = useAppSelector(selectIsAuth);
+  const login = useAppSelector(selectLogin);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const logoutUser = () => {
+    dispatch(logout());
+    setIsVisible(false);
+    navigate('/');
+  };
+
   return (
     <header className="header">
       <Link to="/">
-        <Title level={3}>Some cool header</Title>
+        <Title level={3}>Web CBT</Title>
       </Link>
+      {isAuth && (
+        <Popover
+          className="headerPopover"
+          title={login}
+          visible={isVisible}
+          trigger="click"
+          onVisibleChange={(isVisible) => setIsVisible(isVisible)}
+          content={
+            <div className="popoverContent">
+              <Link to="/settings">
+                <SettingOutlined /> &nbsp;Manage account
+              </Link>
+              <Button
+                className="popoverLogout"
+                danger
+                type="link"
+                onClick={logoutUser}
+              >
+                <LogoutOutlined /> Log out
+              </Button>
+            </div>
+          }
+          placement="bottomRight"
+        >
+          <Avatar className="headerAvatar" icon={<UserOutlined />} />
+          <Button className="headerButton">
+            <UserOutlined />
+            Profile
+          </Button>
+        </Popover>
+      )}
     </header>
   );
 };
--- webcbt-frontend/src/store/user/slice.ts
+++ webcbt-frontend/src/store/user/slice.ts
@@ -1,4 +1,5 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {toast} from 'react-toastify';
 import {User} from '../../types/User';
 
 // Initial state of User slice
@@ -16,9 +17,16 @@ export const userSlice = createSlice({
       state.login = action.payload.login;
       state.accessToken = action.payload.accessToken;
     },
+    logout: (state) => {
+      state.login = '';
+      state.accessToken = undefined;
+      localStorage.removeItem('LOGIN');
+      localStorage.removeItem('TOKEN');
+      toast.warn('Logged out');
+    },
   },
 });
 
-export const {setUser} = userSlice.actions;
+export const {setUser, logout} = userSlice.actions;
 
 export default userSlice.reducer;
