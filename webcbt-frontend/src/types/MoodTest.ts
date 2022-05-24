6960db77d49d9bd3f5613be2be5d825083a89a22
Akmal <akmalmuxiddinov@mail.ru>
2022-05-24 20:59:29 +0200

    Send request on submitting mood test

--- webcbt-frontend/src/types/MoodTest.ts
+++ webcbt-frontend/src/types/MoodTest.ts
@@ -1,7 +1,22 @@
+type userId = 0 | 1;
+
+export type MoodTestCategory =
+  | 'Depression'
+  | 'Anxiety'
+  | 'Addictions'
+  | 'Anger'
+  | 'Relationships'
+  | 'Happiness';
+
 export default interface MoodTest {
-  q1: number;
-  q2: number;
-  q3: number;
-  q4: number;
-  q5: number;
+  question1: number;
+  question2: number;
+  question3: number;
+  question4: number;
+  question5: number;
+}
+
+export interface MoodTestRequest extends MoodTest {
+  userId: userId;
+  category: MoodTestCategory;
 }
