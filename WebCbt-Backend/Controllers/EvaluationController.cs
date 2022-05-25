9fd5ff5340f68dfaeb59fd2c0a8458374883c387
cutieskye <68032402+cutieskye@users.noreply.github.com>
2022-05-25 14:17:58 +0000

    Add Evaluation controller & actions

--- WebCbt-Backend/Controllers/EvaluationController.cs
+++ WebCbt-Backend/Controllers/EvaluationController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
@@ -46,7 +47,94 @@ namespace WebCbt_Backend.Controllers
             return Ok();
         }
 
-        // DELETE: /evaluation/{evaluationId}
+        // GET: /evaluation/findByUserId?userId=5
+        [HttpGet("findByUserId")]
+        public async Task<ActionResult<IEnumerable<Evaluation>>> FindEvaluationsByUserId(int userId)
+        {
+            if (User.Identity?.IsAuthenticated != true)
+            {
+                return Unauthorized();
+            }
+
+            if (_context.Evaluations == null)
+            {
+                return NotFound();
+            }
+
+            return await _context.Evaluations.Where(x => x.UserId == userId).ToListAsync();
+        }
+
+        // GET: /evaluation/5
+        [HttpGet("{evaluationId}")]
+        public async Task<ActionResult<Evaluation>> FindEvaluationById(int evaluationId)
+        {
+            if (User.Identity?.IsAuthenticated != true)
+            {
+                return Unauthorized();
+            }
+
+            if (_context.Evaluations == null)
+            {
+                return NotFound();
+            }
+
+            var evaluation = await _context.Evaluations.FindAsync(evaluationId);
+
+            if (evaluation == null)
+            {
+                return NotFound();
+            }
+
+            if (User.FindFirstValue("userId") != evaluation.UserId.ToString())
+            {
+                return Unauthorized();
+            }
+
+            return evaluation;
+        }
+
+        // PUT: /evaluation/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEvaluation(int id, Evaluation evaluation)
+        {
+            if (User.Identity?.IsAuthenticated != true || User.FindFirstValue("userId") != evaluation.UserId.ToString())
+            {
+                return Unauthorized();
+            }
+
+            if (id != evaluation.EvaluationId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(evaluation).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EvaluationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool EvaluationExists(int id)
+        {
+            return (_context.Evaluations?.Any(e => e.EvaluationId == id)).GetValueOrDefault();
+        }
+
+        // DELETE: /evaluation/5
         [HttpDelete("{evaluationId}")]
         public async Task<IActionResult> DeleteEvaluation(int evaluationId)
         {
