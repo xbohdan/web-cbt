51b6c46d699d6709fa6ef16914b87edbe52016df
threepointsix <akmal.mukh@proton.me>
2022-06-15 15:57:56 +0200

    Fix admin mood tests

--- webcbt-frontend/src/pages/AdminMoodTests/AdminMoodTests.tsx
+++ webcbt-frontend/src/pages/AdminMoodTests/AdminMoodTests.tsx
@@ -1,67 +1,7 @@
 import './AdminMoodTests.css';
 import {Button, Table} from 'antd';
-import {useDeleteMoodTestMutation} from '../../store/services/evaluation';
 import {MoodTestResponse} from '../../types/MoodTest';
 import useAdminMoodTests from '../../hooks/useAdminMoodTests';
-
-const columns = [
-  {
-    title: 'User ID',
-    dataIndex: 'userId',
-    key: 'userId',
-  },
-  {
-    title: 'Evaluation ID',
-    dataIndex: 'evaluationId',
-    key: 'evaluationId',
-  },
-  {
-    title: 'Category',
-    dataIndex: 'category',
-    key: 'category',
-  },
-  {
-    title: 'Question 1',
-    dataIndex: 'question1',
-    key: 'question1',
-  },
-  {
-    title: 'Question 2',
-    dataIndex: 'question2',
-    key: 'question2',
-  },
-  {
-    title: 'Question 3',
-    dataIndex: 'question3',
-    key: 'question3',
-  },
-  {
-    title: 'Question 4',
-    dataIndex: 'question4',
-    key: 'question4',
-  },
-  {
-    title: 'Question 5',
-    dataIndex: 'question5',
-    key: 'question5',
-  },
-  {
-    key: 'delete',
-    // @ts-ignore
-    render: (_, record: MoodTestResponse) => (
-      <Button
-        onClick={async () => {
-          const [deleteMoodTest] = useDeleteMoodTestMutation();
-          await deleteMoodTest(record.evaluationId);
-        }}
-        type="text"
-        danger
-      >
-        Delete
-      </Button>
-    ),
-  },
-];
 
 const AdminMoodTests = () => {
   const {allMoodTests, OnDelete} = useAdminMoodTests();
@@ -70,7 +10,6 @@ const AdminMoodTests = () => {
     <Table
       className="adminMoodTestsTable"
       dataSource={allMoodTests}
-      columns={columns}
     >
       <Table.Column title="User ID" dataIndex="userId" key="userId" />
       <Table.Column
