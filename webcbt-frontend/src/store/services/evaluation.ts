68511991696da96dbf3a757701eacd77eb744064
Akmal <akmalmuxiddinov@mail.ru>
2022-06-15 22:25:41 +0200

    Fix

--- webcbt-frontend/src/store/services/evaluation.ts
+++ webcbt-frontend/src/store/services/evaluation.ts
@@ -1,22 +1,7 @@
 import {MoodTestResponse} from '../../types/MoodTest';
 import { mainApi } from './main';
 
-<<<<<<< HEAD
 export const moodTestApi = mainApi.injectEndpoints({
-=======
-export const moodTestApi = createApi({
-  baseQuery: fetchBaseQuery({
-    baseUrl: 'https://school-se-back.monicz.pl/evaluation/',
-    prepareHeaders: (headers, {getState}) => {
-      // If we have a token, let's use that for authenticated requests
-      const token = getToken(getState() as RootState);
-      if (token) {
-        headers.set('Authorization', `Bearer ${token}`);
-      }
-      return headers;
-    },
-  }),
->>>>>>> 48d5e30d9fbde2d6e3739f3a81fbdc1a14582ba2
   endpoints: (builder) => ({
     getAllMoodTests: builder.mutation<MoodTestResponse[], void>({
       query: () => ({
